d62f842b74431ec301d5b8db577f8aec8eb096ad
Hasan <h.mjabai92@gmail.com>
2023-10-11 20:41:14 +0300

    complted assignment

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
-function isPalindrome(word) {
-  // Write your algorithm here
-}
+
+
 
 /* 
   Add your pseudocode here
@@ -10,16 +9,28 @@ function isPalindrome(word) {
   Add written explanation of your solution here
 */
 
-// You can run `node index.js` to view these console logs
-if (require.main === module) {
-  // add your own custom tests in here
-  console.log("Expecting: true");
-  console.log("=>", isPalindrome("racecar"));
-
-  console.log("");
-
-  console.log("Expecting: false");
-  console.log("=>", isPalindrome("robot"));
+function comp(word) {
+    const wordAr = word.split("")
+    if (wordAr[0] == wordAr[wordAr.length-1]) {
+        return true
+    } else {
+        return false
+    }
+    
 }
+    console.log(comp("mome"))
 
-module.exports = isPalindrome;
+
+// You can run `node index.js` to view these console logs
+// if (require.main === module) {
+//   // add your own custom tests in here
+//   console.log("Expecting: true");
+//   console.log("=>", isPalindrome("racecar"));
+
+//   console.log("");
+
+//   console.log("Expecting: false");
+//   console.log("=>", isPalindrome("robot"));
+// }
+
+// module.exports = isPalindrome;
